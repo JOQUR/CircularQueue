23d69994279de45f95fc8eeef33f9621aa8246f3
kierzek <kierzek@student.agh.edu.pl>
2023-03-19 01:16:47 +0100

    some style changes v2

--- main.c
+++ main.c
@@ -1,46 +1,47 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <malloc.h>
 #include "CircuralBuffer.h"
 
 #define BUFF_SIZE 32
+
+char outBuff[BUFF_SIZE];
 
 int main(int argc, char **argv)
 {
     char* data = argv[1];
     size_t dataSize = BUFF_SIZE;
-    printf("DataSize = %d", dataSize);
     CircularBuffer buffer;
     CircularBuffer_init(&buffer, &dataSize);
+    
     for(int i = 0; i < dataSize; i++)
     {
         CircularBuffer_putData(&buffer, data[i]);
     }
 
     char output;
+
     for(int i = 0; i < (dataSize); i++)
     {
         CircularBuffer_getData(&buffer, &output);
         if(0x00 == output)
             break;
-        printf("Output: %c\n", output);
+
+        outBuff[i] = output;
     }
+
     CircularBuffer_clear(&buffer);
     printf("============================================================\n");
-    data = argv[2];
-    dataSize = BUFF_SIZE;
-    for(int i = 0; i < dataSize; i++)
-    {
-        CircularBuffer_putData(&buffer, data[i]);
-    }
-    for(int i = 0; i < (dataSize); i++)
-    {
-        CircularBuffer_getData(&buffer, &output);
-        if(0x00 == output)
-            break;
-        printf("Output: %c\n", output);
-    }
     CircularBuffer_deinit(&buffer);
+    printf("============================================================\n");
+
+    for(int i = 0; i < BUFF_SIZE; i++)
+    {
+        if(0x00 == outBuff[i])
+            break;
+        
+        printf("\033[31m%c", outBuff[i]);
+        
+    }
+    printf("\033[0m");
+    printf("\n============================================================\n");
 
     return 0;
 }
